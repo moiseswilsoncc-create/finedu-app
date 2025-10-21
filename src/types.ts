bf192a6df82cf938c2f3d7e4a2ad18936f6798ad
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-21 16:28:19 -0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,26 @@
 // Participantes del grupo
 export type Participante = {
   nombre: string;
+  apellido?: string;
+  fechaNacimiento: string;
+  ciudad: string;
+  comuna: string;
   ingresos: number;
   egresos: number;
   metaIndividual: number;
+  correo?: string;
 };
 
 // Datos del usuario solicitante
 export type DatosUsuario = {
   nombre: string;
-  edad: number;
+  apellido: string;
+  fechaNacimiento: string; // formato YYYY-MM-DD
   pais: string;
   ciudad: string;
+  comuna: string;
   correo: string;
+  contraseña: string;
 };
 
 // Datos de institución colaboradora
