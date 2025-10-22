3ba0ace05d58d19e7b3f875ed1cfa591c9dfe03d
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-22 13:33:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Asegura que la raíz sea la carpeta actual
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'public/index.html')
+    }
+  },
   server: {
     port: 3000
   }
