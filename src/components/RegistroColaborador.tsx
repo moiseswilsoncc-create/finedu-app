469daea7e372114c7fc5c454bbdd6f0fc6f1a05b
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-22 20:04:08 -0300

    Create RegistroColaborador.tsx

---
+++ src/components/RegistroColaborador.tsx
@@ -0,0 +1,10 @@
+function RegistroColaborador() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Registro de nuevo colaborador</h2>
+      {/* Aquí va el formulario de inscripción */}
+    </div>
+  );
+}
+
+export default RegistroColaborador;
