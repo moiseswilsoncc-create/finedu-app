7684f99fd503c68829e56971a645fc5e947114e1
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-22 20:02:40 -0300

    Create RegistroUsuario.tsx

---
+++ src/components/RegistroUsuario.tsx
@@ -0,0 +1,10 @@
+function RegistroUsuario() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Registro de nuevo usuario</h2>
+      {/* Aquí va el formulario de inscripción */}
+    </div>
+  );
+}
+
+export default RegistroUsuario;
