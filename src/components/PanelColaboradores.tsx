e8c8d9ca253e538c1238af8d5591059204b27065
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-22 13:51:58 -0300

    Create PanelColaboradores.tsx

---
+++ src/components/PanelColaboradores.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PanelColaboradores() {
+  return (
+    <div>
+      <h2>Panel de Colaboradores</h2>
+      <p>Aquí se mostrará la información de los participantes.</p>
+    </div>
+  )
+}
+
+export default PanelColaboradores
