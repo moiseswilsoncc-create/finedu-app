f9e3a9cd2fb4c465da48c04bd671483aa8d70855
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-23 04:50:12 -0300

    mejorado main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { GrupoProvider } from './context/GrupoContext';
 
-const rootElement = document.getElementById('root') as HTMLElement
-const root = ReactDOM.createRoot(rootElement)
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GrupoProvider>
+        <App />
+      </GrupoProvider>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
