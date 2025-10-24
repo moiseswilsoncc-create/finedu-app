eeaf7693ef84f035464401fc69247fdf638f291d
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-24 00:48:21 -0300

    Update FelicitacionRegistro.tsx

--- src/components/FelicitacionRegistro.tsx
+++ src/components/FelicitacionRegistro.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./FelicitacionRegistro.css"; // Opcional: personaliza estilos si lo deseas
 
 const FelicitacionRegistro = () => {
   return (
