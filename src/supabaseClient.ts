73a48086cbf2c63224eecef1280e7b6828b9033f
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-25 18:34:36 -0300

    ulr y clave supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/utils/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://TU-PROYECTO.supabase.co"; // ← reemplaza con tu URL real
-const supabaseAnonKey = "TU-CLAVE-ANONIMA";            // ← reemplaza con tu anon key
+const supabaseUrl = "https://supabase.com/dashboard/project/ftsbnorudtcyrruubutt"; // ← reemplaza con tu URL real
+const supabaseAnonKey = "Ut!d&4Nrr_BzgJY";            // ← reemplaza con tu anon key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
