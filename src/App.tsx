8175ecad77ce22622e808b41e35acede151367c4
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-26 03:40:43 -0300

    probando 7 cambios de prueba App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TestRender from "./components/TestRender";
 
 function App() {
-  console.log("✅ App.tsx con BrowserRouter sin rutas");
-
   return (
-    <Router>
-      <div
-        style={{
-          padding: "2rem",
-          textAlign: "center",
-          color: "white",
-          backgroundColor: "teal",
-          fontSize: "2rem",
-          borderRadius: "8px",
-          marginTop: "4rem",
-        }}
-      >
-        ✅ BrowserRouter está montado sin rutas
-      </div>
+    <Router basename="/finedu-app">
+      <Routes>
+        <Route path="/" element={<TestRender />} />
+      </Routes>
     </Router>
   );
 }
