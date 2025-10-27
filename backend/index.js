13152bfa965d17c277d1f0d6a1c51bff8be37a99
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-27 15:50:49 -0300

    endpoint /guardar-aporte conectado a Supabase index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
+// Rutas institucionales
 const solicitudColaboradorRoutes = require("./routes/solicitudColaborador");
+const ofertasRoutes = require("./routes/ofertas");
+const aportesRoutes = require("./routes/aportes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +13,10 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Activar rutas
+// Activar rutas institucionales
 app.use("/api", solicitudColaboradorRoutes);
+app.use("/api", ofertasRoutes);
+app.use("/api", aportesRoutes);
 
 // Ruta base
 app.get("/", (req, res) => {
