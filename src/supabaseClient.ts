effad4f328ada92216e463eb842dadc41c52a31e
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-28 16:53:43 -0300

    actualizado vite supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,8 @@
 // finedu-app/src/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-// Usamos variables de entorno públicas para que estén disponibles en el navegador
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// Usamos variables de entorno públicas para Vite
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
