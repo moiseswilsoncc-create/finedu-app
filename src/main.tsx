8d1c73217f60cf5553243c3d45f74887af87bc6f
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-28 14:18:31 -0300

    actualizar main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { GrupoProvider } from './context/GrupoContext';
 
 console.log("✅ main.tsx ejecutado");
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GrupoProvider>
+        <App />
+      </GrupoProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
