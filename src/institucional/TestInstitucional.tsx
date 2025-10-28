d4c29f0c84057dd63e34922d7b2adcc2e48075ca
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-28 12:08:16 -0300

    prueba  TestInstitucional.tsx

---
+++ src/institucional/TestInstitucional.tsx
@@ -0,0 +1,10 @@
+const TestInstitucional = () => {
+  console.log("🧪 TestInstitucional se está montando");
+  return (
+    <div style={{ padding: "2rem", fontSize: "1.5rem", color: "blue" }}>
+      🧪 TestInstitucional cargado correctamente
+    </div>
+  );
+};
+
+export default TestInstitucional;
