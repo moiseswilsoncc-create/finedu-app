f77def403a75fc95f1d52aa51f312cfa8730e01e
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-28 11:55:32 -0300

    actualizada index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// ✅ Verifica que el div#root exista
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("❌ No se encontró el elemento #root en index.html");
+}
