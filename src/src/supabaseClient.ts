2b612871dfdcd9ba86ed8554a4cac2f85d1c66ae
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-11-01 04:04:36 -0300

    actualizado supabaseClient.ts

--- src/src/supabaseClient.ts
+++ src/src/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// 🔐 Claves institucionales activas
+const SUPABASE_URL = "https://ftsnonrudtcyrruubutt.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBiYXNlIiwic3ViIjoiYXVub24iLCJ2ZXJzaW9uIjoxLCJpYXQiOjE2NzUwODI1NzIsImV4cCI6MTY4MzYyODU3MiwiYXR0cmlidXRlcyI6eyJwcm9qZWN0X2lkIjoiY2xpY2tzdGFydGVyIn19.qT5600RcYkK1R8LsNi14t1ZBVf1ZanfGkxd8XoUeg9sP";
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
   throw new Error("❌ Faltan variables de entorno para Supabase en frontend.");
