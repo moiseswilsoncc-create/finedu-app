925e16956e3d8a2a62dfad566a3c81aa2cb528fb
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-11-02 18:30:34 -0300

    actualizada y ordenada  App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // Archivo: src/App.tsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 // 🧠 Pantalla raíz y flujo de ingreso
 import Bienvenida from "./components/Bienvenida";
@@ -9,20 +9,15 @@ import LoginUsuario from "./components/LoginUsuario";
 import PanelUsuario from "./components/PanelUsuario";
 
 // 🧩 Módulo Finanzas (corazón del sistema)
-import Finanzas from "./components/Finanzas";
+import Finanza from "./components/Finanza";
 import Ingresos from "./components/Ingresos";
 import Egresos from "./components/Egresos";
 import ResumenFinanciero from "./components/ResumenFinanciero";
+import SimuladorCreditos from "./components/SimuladorCreditos";
 
 // 🧩 Otros módulos de usuario
-import RegistroAhorro from "./components/RegistroAhorro";
-import SimuladorInversion from "./components/SimuladorInversion";
-import TestFinanciero from "./components/TestFinanciero";
 import VistaGrupal from "./components/VistaGrupal";
 import AdminGrupo from "./components/AdminGrupo";
-import EvaluadorCredito from "./components/EvaluadorCredito";
-import PanelOfertas from "./components/PanelOfertas";
-import DatosOfertas from "./components/DatosOfertas";
 
 // 🧩 Colaboradores
 import RegistroColaborador from "./components/RegistroColaborador";
@@ -30,7 +25,8 @@ import IngresoColaborador from "./components/IngresoColaborador";
 import LoginColaborador from "./components/LoginColaborador";
 import PanelColaboradores from "./components/PanelColaboradores";
 import InvitacionColaboradores from "./components/InvitacionColaboradores";
-
+import OfertasColaborador from "./components/OfertasColaborador";
+import PublicarOfertaColaborador from "./components/PublicarOfertaColaborador";
 // 🧩 Institucional
 import DashboardInstitucional from "./institucional/DashboardInstitucional";
 import EditorEstadoArchivos from "./institucional/EditorEstadoArchivos";
@@ -38,55 +34,55 @@ import EditorTrazabilidad from "./institucional/EditorTrazabilidad";
 import MetricaSupabase from "./institucional/MetricaSupabase";
 import TestInstitucional from "./institucional/TestInstitucional";
 
-// 🧩 Menú de módulos
+// 🧩 Menú y navegación
 import MenuModulos from "./components/MenuModulos";
+import Navbar from "./components/Navbar";
 
 console.log("🧼 App.tsx actualizado: mapa maestro con todas las rutas activas");
-
 const App: React.FC = () => {
   return (
-    <Routes>
-      {/* Bienvenida */}
-      <Route path="/" element={<Bienvenida />} />
+    <Router>
+      <Navbar />
+      <MenuModulos />
+      <Routes>
+        {/* Bienvenida */}
+        <Route path="/" element={<Bienvenida />} />
 
-      {/* Usuarios */}
-      <Route path="/registro-usuario" element={<RegistroUsuario />} />
-      <Route path="/login-usuario" element={<LoginUsuario />} />
-      <Route path="/panel-usuario" element={<PanelUsuario />} />
+        {/* Usuarios */}
+        <Route path="/registro-usuario" element={<RegistroUsuario />} />
+        <Route path="/login-usuario" element={<LoginUsuario />} />
+        <Route path="/panel-usuario" element={<PanelUsuario />} />
 
-      {/* Finanzas */}
-      <Route path="/finanzas" element={<Finanzas />} />
-      <Route path="/finanzas/ingresos" element={<Ingresos />} />
-      <Route path="/finanzas/egresos" element={<Egresos />} />
-      <Route path="/finanzas/resumen" element={<ResumenFinanciero />} />
+        {/* Finanzas */}
+        <Route path="/finanzas" element={<Finanza pais="Chile" />} />
+        <Route path="/finanzas/ingresos" element={<Ingresos />} />
+        <Route path="/finanzas/egresos" element={<Egresos />} />
+        <Route path="/finanzas/resumen" element={<ResumenFinanciero />} />
+        <Route path="/finanzas/creditos" element={<SimuladorCreditos />} />
+        {/* Otros módulos de usuario */}
+        <Route path="/vista-grupal" element={<VistaGrupal />} />
+        <Route path="/admin-grupo" element={<AdminGrupo />} />
 
-      {/* Otros módulos de usuario */}
-      <Route path="/registro-ahorro" element={<RegistroAhorro />} />
-      <Route path="/simulador-inversion" element={<SimuladorInversion />} />
-      <Route path="/test-financiero" element={<TestFinanciero />} />
-      <Route path="/vista-grupal" element={<VistaGrupal />} />
-      <Route path="/admin-grupo" element={<AdminGrupo />} />
-      <Route path="/evaluador-credito" element={<EvaluadorCredito />} />
-      <Route path="/panel-ofertas" element={<PanelOfertas />} />
-      <Route path="/datos-ofertas" element={<DatosOfertas />} />
+        {/* Colaboradores */}
+        <Route path="/registro-colaborador" element={<RegistroColaborador />} />
+        <Route path="/ingreso-colaborador" element={<IngresoColaborador />} />
+        <Route path="/login-colaborador" element={<LoginColaborador />} />
+        <Route path="/panel-colaboradores" element={<PanelColaboradores />} />
+        <Route path="/invitacion-colaboradores" element={<InvitacionColaboradores />} />
+        <Route path="/ofertas" element={<OfertasColaborador />} />
+        <Route path="/publicar" element={<PublicarOfertaColaborador />} />
 
-      {/* Colaboradores */}
-      <Route path="/registro-colaborador" element={<RegistroColaborador />} />
-      <Route path="/ingreso-colaborador" element={<IngresoColaborador />} />
-      <Route path="/login-colaborador" element={<LoginColaborador />} />
-      <Route path="/panel-colaboradores" element={<PanelColaboradores />} />
-      <Route path="/invitacion-colaboradores" element={<InvitacionColaboradores />} />
+        {/* Institucional */}
+        <Route path="/dashboard-institucional" element={<DashboardInstitucional />} />
+        <Route path="/editor-estado" element={<EditorEstadoArchivos />} />
+        <Route path="/editor-trazabilidad" element={<EditorTrazabilidad />} />
+        <Route path="/metrica-supabase" element={<MetricaSupabase />} />
+        <Route path="/test-institucional" element={<TestInstitucional />} />
 
-      {/* Institucional */}
-      <Route path="/dashboard-institucional" element={<DashboardInstitucional />} />
-      <Route path="/editor-estado" element={<EditorEstadoArchivos />} />
-      <Route path="/editor-trazabilidad" element={<EditorTrazabilidad />} />
-      <Route path="/metrica-supabase" element={<MetricaSupabase />} />
-      <Route path="/test-institucional" element={<TestInstitucional />} />
-
-      {/* Menú de módulos */}
-      <Route path="/menu-modulos" element={<MenuModulos />} />
-    </Routes>
+        {/* Menú de módulos */}
+        <Route path="/menu-modulos" element={<MenuModulos />} />
+      </Routes>
+    </Router>
   );
 };
 
