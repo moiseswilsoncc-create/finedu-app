27e605d010c6b8b80d90ed8117ee384b33890f8c
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-11-03 01:25:13 -0300

    Update ActivarColaborador.tsx

--- src/colaboradores/ActivarColaborador.tsx
+++ src/colaboradores/ActivarColaborador.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "../supabaseClient";
 
 const supabase = createClient("https://ftsbnorudtcyrrubutt.supabase.co", "TU_API_KEY");
 
